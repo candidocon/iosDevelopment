758327a448ebd67f12e93edb28ca198400576d83
candido concepcion <candidoconcepcion@candidos-iMac.attlocal.net>
2019-09-24 17:39:09 -0400

    quiz app v1

---
+++ Exercises/Quiz/Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by candido concepcion on 8/31/19.
+//  Copyright © 2019 COP4655. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
